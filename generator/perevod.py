8929182b495b052aa2107a19c169bcd29a34b144
loonfee <markushina0l0@gmail.com>
2021-10-16 11:57:02 +0300

    GENERATOR Added pep8 new line

--- generator/perevod.py
+++ generator/perevod.py
@@ -2,6 +2,8 @@ with open("leetcode", "r") as file:
     header = file.readline()
     link = file.readline()
     code = file.read()
+
+
 with open("result.txt", "w") as file:
     print("##", header, sep= " ", end= '\n', file = file)
     print(link, end='\n', file=file)
